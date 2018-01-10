a5e1a486eee83a7e419e5e9840201ddc92002db8
Alexander Schwartzberg <aeksco@gmail.com>
2018-01-10 18:13:17 -0500

    Improved handling of relational data in RecordShow page

--- src/containers/record_show/components/layout.vue
+++ src/containers/record_show/components/layout.vue
@@ -7,8 +7,14 @@
         <div class="card card-body bg-dark color-light border-light">
 
           <div class="row">
-            <div class="col-lg-12">
+            <div class="col-lg-8">
               <h2>{{schema.label}} Detail</h2>
+            </div>
+            <div class="col-lg-4 text-right">
+              <a class="btn btn-outline-warning btn-sm" :href="'#/schemas/' + schema._id + '/records/' + record._id + '/edit'">
+                <i class="fa fa-fw fa-pencil mr-1"></i>
+                Edit
+              </a>
             </div>
           </div>
 
@@ -22,7 +28,16 @@
                         {{attr.label}}
                       </strong>
                     </td>
-                    <td>{{record.attributes[attr.identifier]}}</td>
+                    <td>
+                      <a v-if="attr.datatype === 'SCHEMA'" :href="getLinkedSchemaHref(attr, record.attributes[attr.identifier])">
+                        {{ getLinkedSchemaLabel(attr, record.attributes[attr.identifier]) }}
+                      </a>
+                      <span v-else-if="attr.datatype === 'BOOL'">
+                        <i class="fa fa-fw fa-check-square-o" v-if="record.attributes[attr.identifier]"></i>
+                        <i class="fa fa-fw fa-square-o" v-if="!record.attributes[attr.identifier]"></i>
+                      </span>
+                      <span v-else>{{record.attributes[attr.identifier]}}</span>
+                    </td>
                   </tr>
                 </tbody>
               </table>
@@ -33,12 +48,23 @@
       </div>
     </div>
 
-    <div class="row mt-4" v-for="rel in schema.relations" :key="rel._id">
+    <div class="row mt-4" v-for="rel in schema.relations" :key="rel._id" v-if="rel.type === 'HAS_MANY'">
 
       <!-- Relation Viewer -->
       <div class="col-lg-12">
         <div class="card card-body bg-dark color-light border-light">
-          <p class="lead mb">Related {{ relatedSchemaName(rel) }}</p>
+          <div class="row">
+            <div class="col-lg-8">
+              <p class="lead mb">Related {{ relatedSchemaName(rel) }}</p>
+            </div>
+            <div class="col-lg-4 text-right">
+              <a class="btn btn-outline-success btn-sm" :href="'#/schemas/' + relatedSchema(rel)._id + '/records/new'">
+                <i class="fa fa-fw fa-plus mr-1"></i>
+                New
+              </a>
+            </div>
+          </div>
+
           <RecordTable :schema="relatedSchema(rel)" :records="getRelatedRecords(rel)"/>
         </div>
       </div>
@@ -61,6 +87,22 @@ export default {
     RecordTable
   },
   methods: {
+    getLinkedSchemaHref (attr, record_id) { // TODO - abstract to component
+      let allRecords = store.getters['record/collection']
+      let allSchemas = store.getters['schema/collection']
+      let record = _.find(allRecords, { _id: record_id })
+      if (!record) return
+      let schema = _.find(allSchemas, { _id: record.schema_id })
+      return '#/schemas/' + schema._id + '/records/' + record._id
+    },
+    getLinkedSchemaLabel (attr, record_id) { // TODO - abstract to component
+      let allRecords = store.getters['record/collection']
+      let record = _.find(allRecords, { _id: record_id })
+      if (!record) return
+
+      let identifier = attr.datatypeOptions.schema_attribute_identifier
+      return record.attributes[identifier]
+    },
     relatedSchema (relation) { // TODO - this should be moved into a getter
       let allSchemas = store.getters['schema/collection']
       let relatedSchema = _.find(allSchemas, { _id: relation.schema_id })
